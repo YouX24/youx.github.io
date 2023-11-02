39b4418cde6e0f6a307c44a7e31bca093e9e505e
Yousae <yousaex24@gmail.com>
2023-11-02 16:03:32 -0500

    added experience timeline

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,57 @@
-import ExperienceCard from "./ExperienceCard"
+// import ExperienceCard from "./ExperienceCard"
+import optumLogo from '../assets/optum-logo.svg'
+import uwecLogo from '../assets/uwec-logo.png'
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component"
+import 'react-vertical-timeline-component/style.min.css';
 
 const Experience = () => {
     return (
-        <div className="flex justify-center min-h-screen px-6 py-12 sm:px-12">
+        <div className="flex justify-center min-h-screen px-6 py-12 sm:px-12 bg-slate-500">
             <div className="flex items-center flex-col">
-                <h2 className="text-4xl mb-10">EXPERIENCE</h2>
+                <h2 className="text-4xl mb-10 text-white">EXPERIENCE</h2>
                 <div className="flex justify-center items-center">
-                    <ExperienceCard/>
+                    {/* <ExperienceCard/> */}
+                    <VerticalTimeline>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                            date="June 2023 - August 2023"
+                            iconStyle={{ background: '#fff', color: '#fff' }}
+                            icon={
+                                <div className='w-full h-full flex justify-center items-center'>
+                                    <img src={optumLogo} alt="company" className="object-contain"/>
+                                </div>}
+                        >
+                            <h3 className="vertical-timeline-element-title text-lg">Software Engineer Intern</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Optum</h4>
+                            <ul className="list-[circle] mt-5 ml-5 space-y-2">
+                                <li>Reduced manual labor and optimized workflow efficiency by developing a full-stack file validation application to automate file validation and duplicate checks.</li>
+                                <li>Seamlessly detect errors in files consisting of over 1600 values, by implementing an efficient algorithm to parse extensive files to find errors.</li>
+                                <li>Elevated backend API code quality, achieving over 84% code coverage and minimizing production issues through writing comprehensive unit tests.</li>
+                                <li>Streamlined the deployment cycle, reduced deployment time, and fostered faster iterations and collaborative teamwork, by implementing CI pipeline with GitHub Actions.</li>
+                            </ul>
+                        </VerticalTimelineElement>
+                        <VerticalTimelineElement
+                            className="vertical-timeline-element--work"
+                            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+                            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+                            date="November 2020 - March 2021"
+                            iconStyle={{ background: '#fff', color: '#fff' }}
+                            icon={
+                                <div className='w-full h-full flex justify-center items-center'>
+                                    <img src={uwecLogo} alt="company"/>
+                                </div>}
+                        >
+                            <h3 className="vertical-timeline-element-title text-lg">Computer Science Administrator</h3>
+                            <h4 className="vertical-timeline-element-subtitle">Univerity of Wisconsin-Eau Claire - Learning & Technology Services</h4>
+                            <ul className="list-[circle] mt-5 ml-5 space-y-2">
+                                <li>Ensured seamless server access for students and faculty, by managing and updating University CentOS and web servers.</li>
+                                <li>Facilitated problem-solving for both faculty and students, by providing timely software support.</li>
+                                <li>Honed user support abilities and explored innovative technologies for potential integration in CS courses.</li>
+                            </ul>
+                        </VerticalTimelineElement>
+                    </VerticalTimeline>
                 </div>
             </div>
         </div>
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,10 +3,15 @@ const Intro = () => {
     let introArr = ["Hey there!", "Nice to meet you!", "My name is Yousae", "I'm a student at the University of Wisconsin-Eau Claire", "I'm studying Computer Science and Information Systems", "I'm interested in software engineering", "Thanks for visiting"]
 
     return (
-        <div className="flex justify-center items-center sm:flex-row flex-col min-h-screen gap-5 sm:gap-12">
+        <div className="flex justify-center items-center sm:flex-row flex-col min-h-screen gap-5 sm:gap-12 bg-[#659DBD]">
             <img src={portrait} alt="" className="sm:w-52 w-48 rounded-full"/>
             <div className="flex justify-center items-center flex-col gap-y-5 text-center">
                 <h1 className="text-8xl">Yousae Xiong</h1>
+                <div className='flex gap-3'>
+                    <a className="" href="https://www.linkedin.com/in/yousaexiong/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+                    <a className="" href="https://github.com/YouX24" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
+                    <a className="" href="mailto:x.yousae@gmail.com"><i className="fa-solid fa-envelope"></i></a>
+                </div>
                 <p className="text-2xl">{introArr[0]}</p>
             </div>
         </div>
