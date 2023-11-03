9975abafb1aac1353fe70a68815797c59d9608a2
Yousae <yousaex24@gmail.com>
2023-11-02 22:21:24 -0500

    test

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,7 +8,7 @@ const Intro = () => {
             <div className="flex justify-center items-center flex-col gap-y-5 text-center">
                 <h1 className="text-8xl">Yousae Xiong</h1>
                 <div className='flex gap-5'>
-                    {/* <a className="" href="https://www.linkedin.com/in/yousaexiong/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin text-2xl"></i></a> */}
+                    <a className="" href="https://www.linkedin.com/in/yousaexiong/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin text-2xl"></i></a>
                     <a className="" href="https://github.com/YouX24" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github text-2xl"></i></a>
                     <a className="" href="mailto:x.yousae@gmail.com"><i className="fa-solid fa-envelope text-2xl"></i></a>
                 </div>
