1fe072c085d9a01e2d8231a0cb2f0a93872bb92c
Yousae <yousaex24@gmail.com>
2023-11-02 22:34:31 -0500

    added gh action

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
                 <h1 className="text-8xl">Yousae Xiong</h1>
                 <div className='flex gap-5'>
                     <a className="" href="https://www.linkedin.com/in/yousaexiong/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin text-2xl"></i></a>
-                    {/* <a className="" href="https://github.com/YouX24" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github text-2xl"></i></a> */}
+                    <a className="" href="https://github.com/YouX24" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github text-2xl"></i></a>
                     <a className="" href="mailto:x.yousae@gmail.com"><i className="fa-solid fa-envelope text-2xl"></i></a>
                 </div>
                 <p className="text-2xl">{introArr[0]}</p>
