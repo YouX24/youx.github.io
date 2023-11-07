c0ac3cb519741f43d754c8719c3cbb24c8bc8560
Yousae <yousaex24@gmail.com>
2023-11-06 19:43:31 -0600

    updated gh action

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "youx.github.io" // REQUIRED: name of repository
+  base: "/youx.github.io" // REQUIRED: name of repository
 })
